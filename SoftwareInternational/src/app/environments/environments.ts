528cec920be0dceee422fd2ca6384f668fb3fc9c
luka lobzhanidze <luukitto@gmail.com>
2024-06-27 18:42:21 +0400

    nestjs and firebase config done

---
+++ SoftwareInternational/src/app/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBGZNmK78WvEPX4DGU6AEyGYm2veUiea2g",
+    authDomain: "sales-management-applica-862e8.firebaseapp.com",
+    projectId: "sales-management-applica-862e8",
+    storageBucket: "sales-management-applica-862e8.appspot.com",
+    messagingSenderId: "412542878376",
+    appId: "1:412542878376:web:b2417897bc150120bb789d",
+    measurementId: "G-DGEFQ3QQ5W"
+  }
+};
